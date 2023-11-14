1bf259b855262be77a2df4ea36433bb5afbe37cd
VBog <vboginc@yandex.ru>
2023-11-14 17:23:10 +0500

    all

---
+++ lab/firstlab/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace firstlab
+{
+    internal class Program
+    {
+        private static int Calculate(List<int> numbers, List<string> signs)
+        {
+            for (int i = 0; i < signs.Count; i++)
+            {
+                if (signs[i] == "*")
+                {
+                    numbers[i] *= numbers[i + 1];
+                    numbers.RemoveAt(i + 1);
+                    signs.RemoveAt(i);
+                    i -= 1;
+                }
+                else if (signs[i] == "/")
+                {
+                    numbers[i] /=  numbers[i + 1];
+                    signs.RemoveAt(i);
+                    numbers.RemoveAt(i + 1);
+                    i -= 1;
+                }
+            }
+            for (int i = 0; i < signs.Count; i++)
+            {
+                if (signs[i] == "+")
+                {
+                    numbers[i] += numbers[i + 1];
+                    signs.RemoveAt(i);
+                    numbers.RemoveAt(i+1);
+                    i -= 1;
+                }
+                else if (signs[i] == "-")
+                {
+                    numbers[i] -= numbers[i + 1];
+                    signs.RemoveAt(i);
+                    numbers.RemoveAt(i + 1);
+                    i -= 1;
+                }
+            }
+            return numbers[0];
+        }
+
+        private static void FindBrackets(string expression)
+        {
+            int openBracketIndex = expression.IndexOf('(');
+            if (openBracketIndex != -1)
+            {
+                int closeBracketIndex = expression.LastIndexOf(')');
+                if (closeBracketIndex != -1)
+                {
+                    string expressionInBrackets = expression.Substring(openBracketIndex + 1, closeBracketIndex - openBracketIndex - 1);
+                    int resultInBrackets = CalculateExpressionInBrackets(expressionInBrackets);
+                    expression = expression.Remove(openBracketIndex, closeBracketIndex - openBracketIndex + 1);
+                    expression = expression.Insert(openBracketIndex, resultInBrackets.ToString());
+                }
+            }
+        }
+        private static int CalculateExpressionInBrackets(string expression)
+        {
+            List<int> numbers = new List<int>();
+            List<string> signs = new List<string>();
+            return Calculate(WriteNumbersInList(expression, numbers), WriteSignsInList(expression, signs));
+        }
+        private static List<int> WriteNumbersInList(string text, List<int> numbers)
+        {
+            string[] strNums = TextWithOutSigns(text).Split();
+            foreach (var strNum in strNums)
+                if (int.TryParse(strNum, out int num))
+                    numbers.Add(num);
+            return numbers;
+        }
+
+        private static string TextWithOutSigns(string texts)
+        {
+            foreach (var c in texts)
+                if (char.IsSymbol(c) || char.IsPunctuation(c))
+                    texts = texts.Replace(c, ' ');
+            return texts;
+        }
+        private static List<string> WriteSignsInList(string expression, List<string> signs)
+        {
+            foreach (var c in expression)
+                if ((char.IsSymbol(c) || char.IsPunctuation(c)) && (c != '(' && c != ')'))
+                {
+                    signs.Add(c.ToString());
+                    expression = expression.Replace(c, ' ');
+                }
+            return signs;
+        }
+        public static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+            List<int> numbers = new List<int>();
+            List<string> signs = new List<string>();
+            expression = expression.Replace(" ", "");
+            FindBrackets(expression);
+            WriteSignsInList(expression, signs);
+            WriteNumbersInList(expression, numbers);
+            Console.Write("Список чисел: ");
+            foreach (var number in numbers)
+                Console.Write(number + " ");
+            Console.WriteLine();
+            Console.WriteLine("Список знаков: " + string.Join(" ", signs.ToArray()));
+            Console.WriteLine("Результат: " + Calculate(numbers, signs));
+            
+        }
+    }
+}
---
+++ rpn/rpn/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+
+class InfixToRPNCalculator
+{
+    private static int GetOperatorPriority(string op)
+    {
+        switch (op)
+        {
+            case "+":
+            case "-":
+                return 1;
+            case "*":
+            case "/":
+                return 2;
+            default:
+                return 0;
+        }
+    }
+
+    public static string ConvertToRPN(string infixExpression)
+    {
+        string[] infixTokens = infixExpression.Split(' ');
+        Stack<string> operatorStack = new Stack<string>();
+        List<string> rpnTokens = new List<string>();
+        foreach (string token in infixTokens)
+        {
+            if (double.TryParse(token, out _))
+                rpnTokens.Add(token);
+            else if (token == "(")
+                operatorStack.Push(token);
+            else if (token == ")")
+            {
+                while (operatorStack.Count > 0 && operatorStack.Peek() != "(")
+                    rpnTokens.Add(operatorStack.Pop());
+                operatorStack.Pop();
+            }
+            else
+            {
+                while (operatorStack.Count > 0 && GetOperatorPriority(operatorStack.Peek()) >= GetOperatorPriority(token))
+                    rpnTokens.Add(operatorStack.Pop());
+                operatorStack.Push(token);
+            }
+        }
+        while (operatorStack.Count > 0)
+            rpnTokens.Add(operatorStack.Pop());
+        return string.Join(" ", rpnTokens.ToArray());
+    }
+
+    public static double EvaluateRPN(string rpnExpression)
+    {
+        string[] rpnTokens = rpnExpression.Split(' ');
+        Stack<double> stack = new Stack<double>();
+        foreach (string token in rpnTokens)
+        {
+            if (double.TryParse(token, out double operand))
+                stack.Push(operand);
+            else
+            {
+                double operand2 = stack.Pop();
+                double operand1 = stack.Pop();
+                double result = PerformOperation(token, operand1, operand2);
+                stack.Push(result);
+            }
+        }
+
+        return stack.Pop();
+    }
+
+    private static double PerformOperation(string operatorToken, double operand1, double operand2)
+    {
+        switch (operatorToken)
+        {
+            case "+":
+                return operand1 + operand2;
+            case "-":
+                return operand1 - operand2;
+            case "*":
+                return operand1 * operand2;
+            case "/":
+                if (operand2 == 0)
+                    throw new DivideByZeroException("Попытка деления на ноль.");
+                
+                return operand1 / operand2;
+            default:
+                throw new ArgumentException("Недопустимый оператор: " + operatorToken);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        string infixExpression = "1,5 * ( 1250 - 0,7 ) + ( 25 - 2 * ( 5 - 2 ) ) - 6 / 2";
+        string rpnExpression = ConvertToRPN(infixExpression);
+        Console.WriteLine($"Выражение в ОПЗ: {rpnExpression}");
+        Console.WriteLine($"Результат: {EvaluateRPN(rpnExpression)}");
+    }
+}
